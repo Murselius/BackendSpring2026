6c743ed32b002e02a81b9836ef6c6cc342c2f243
bhu608@myy.haaga-helia.fi <bhu608@myy.haaga-helia.fi>
2026-02-04 13:40:49 +0200

    Created bases of needed files for further modification

--- Bookstore/src/main/java/bookstore/bookstore/model/Book.java
+++ Bookstore/src/main/java/bookstore/bookstore/model/Book.java
@@ -1,7 +1,16 @@
 package bookstore.bookstore.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
+    @Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
     private String title;
     private String author;
     private int publicationYear;
@@ -9,6 +18,7 @@ public class Book {
     private double price;
 
     public Book(String title, String author, int publicationYear, String isbn, double price) {
+        super();
         this.title=title;
         this.author=author;
         this.publicationYear=publicationYear;
@@ -23,6 +33,14 @@ public class Book {
         this.isbn="";
         this.price=0;
     }
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
     public String getTitle() {
         return title;
--- Bookstore/src/main/java/bookstore/bookstore/web/BookController.java
+++ Bookstore/src/main/java/bookstore/bookstore/web/BookController.java
@@ -1,16 +1,23 @@
 package bookstore.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import bookstore.bookstore.model.BookRepository;
 
 @Controller
 @ResponseBody
 public class BookController {
-
-    @RequestMapping("/index")
-    public String returnMainMessage() {
-        return "This is the main page";
+	@Autowired
+	private BookRepository repository; 
+	
+    @RequestMapping(value= {"/", "/booklist"})
+    public String studentList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
-
+  
 }
