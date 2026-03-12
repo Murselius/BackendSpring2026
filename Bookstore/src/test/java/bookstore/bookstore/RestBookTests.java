4cc5bc029dfafdb677c5c69fa8d670364baea380
bhu608@myy.haaga-helia.fi <bhu608@myy.haaga-helia.fi>
2026-03-12 21:06:04 +0200

    changed postgresql address to openshift

--- Bookstore/src/test/java/bookstore/bookstore/RestBookTests.java
+++ Bookstore/src/test/java/bookstore/bookstore/RestBookTests.java
@@ -1,5 +1,4 @@
 package bookstore.bookstore;
-import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
